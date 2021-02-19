31f27b8b76da0ae2bd081526c2cff1e8f5e5322c
tasjarutas <tasjarutas@gmail.com>
2021-02-19 18:12:28 +0000

    Added code to demonstrate the simple for loop.

---
+++ basics/repetitions/for_loop/count_down.py
@@ -0,0 +1,5 @@
+print("How far are we from the cave?")
+steps = int(input())
+for step in range(steps,0,-1):
+    print(f"{step} steps remaining")
+print("We have reached the cave!")
---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,14 @@
+print("How many mountains should I display?")
+mountains = int(input())
+print("Displaying...")
+
+for mountain in range(mountains):
+      print("""
+           __
+          /  \\_  
+         /^    \\
+        /  ^    \\_
+      _/ ^ ^     ^\\
+     /  ^     ^    \\
+
+  """)
