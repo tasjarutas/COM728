3a44b5f35b1fd35176b80f6520cbc05ff68807e7
tasjarutas <tasjarutas@gmail.com>
2021-03-24 15:33:59 +0000

    Added code to update a record into the database.

--- persistence/update_one.py
+++ persistence/update_one.py
@@ -22,19 +22,17 @@ def display_bot_from_db(bot_id):
 def get_bot_details_from_user():
     print("What would you like to change:")
     field = input()
-    print(f"What is the new value for {field}")
+    print(f"What is the new value for {field}:")
     value = int(input())
     return [field, value]
 
 
-def update_bot_in_db(data):
+def update_bot_in_db(data, bot_id):
     db = sqlite3.connect("../persistence/sqlite/bots.db")
     cursor = db.cursor()
-    print(data[0])
-    print(data[1])
-    sql = "UPDATE bots SET ?=?"
-    values = [data[0], data[1]]
-    cursor.execute(sql, values)
+    sql = "UPDATE bots SET " + str(data[0]) + " = " + str(data[1]) + " WHERE id = " + bot_id
+    # values = [data[0], data[1], bot_id]
+    cursor.execute(sql)
     db.commit()
     db.close()
 
@@ -46,7 +44,7 @@ def run():
     display_bot_from_db(bot_id)
 
     data = get_bot_details_from_user()
-    update_bot_in_db(data)
+    update_bot_in_db(data, bot_id)
 
 
 if __name__ == "__main__":
