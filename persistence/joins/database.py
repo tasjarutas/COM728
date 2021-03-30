9c14a9c4ac0b91828a7beade487c3f03e8fdcb50
tasjarutas <tasjarutas@gmail.com>
2021-03-30 17:38:45 +0100

    Added code to retrieve data using a Left Outer Join, a Right Outer Join,and  a full outer join.

--- persistence/joins/database.py
+++ persistence/joins/database.py
@@ -41,9 +41,10 @@ def display_product_supplier_locations():
     db = sqlite3.connect("catalogue.db")
     cursor = db.cursor()
 
-    sql = """SELECT product.name, supplier.name
-            FROM product 
-            INNER JOIN supplier  ON product.supplier_id = supplier.id"""
+    sql = """SELECT product.name, supplier.name, location.city, location.country
+FROM product 
+INNER JOIN supplier  ON product.supplier_id = supplier.id
+INNER JOIN location ON supplier.location_id = location.id"""
     cursor.execute(sql)
     all_records = cursor.fetchall()
     db.close()
@@ -51,4 +52,86 @@ def display_product_supplier_locations():
     print("The suppliers for each product are as follows: ")
     for record in all_records:
         print(f"Product: {record[0]}", end=', ')
-        print(f"Supplier: {record[1]}")
+        print(f"Supplier: {record[1]}", end =', ')
+        print(f"Supplier Location: {record[2]}", end=', ')
+        print(f"{record[3]}")
+
+
+def display_products_missing_suppliers():
+    db = sqlite3.connect("catalogue.db")
+    cursor = db.cursor()
+
+    sql = """SELECT product.name, supplier.name, location.city, location.country
+FROM product 
+LEFT OUTER JOIN supplier  ON product.supplier_id = supplier.id
+LEFT OUTER JOIN location ON supplier.location_id = location.id"""
+    cursor.execute(sql)
+    all_records = cursor.fetchall()
+    db.close()
+
+    print("The suppliers for each product are as follows: ")
+    for record in all_records:
+        print(f"Product: {record[0]}", end=', ')
+        print(f"Supplier: {record[1]}", end=', ')
+        print(f"Supplier Location: {record[2]}", end=', ')
+        print(f"{record[3]}")
+
+
+def display_suppliers_missing_products():
+    db = sqlite3.connect("catalogue.db")
+    cursor = db.cursor()
+
+    sql = """SELECT product.name, supplier.name
+FROM supplier 
+LEFT OUTER JOIN product ON supplier.id  = product.supplier_id 
+"""
+    cursor.execute(sql)
+    all_records = cursor.fetchall()
+    db.close()
+
+    print("The suppliers for each product are as follows: ")
+    for record in all_records:
+        print(f"Supplier: {record[1]}", end=', ')
+        print(f"Product: {record[0]}")
+
+
+def display_missing_data():
+    db = sqlite3.connect("catalogue.db")
+    cursor = db.cursor()
+
+    sql1 = """SELECT product.name, supplier.name
+FROM product
+LEFT OUTER JOIN supplier  ON product.supplier_id = supplier.id
+UNION
+SELECT product.name, supplier.name
+FROM supplier 
+LEFT OUTER JOIN product ON supplier.id  = product.supplier_id
+    """
+
+    sql2 = """SELECT supplier.name, location.city, location.country
+FROM supplier
+LEFT OUTER JOIN location  ON supplier.location_id = location.id
+UNION
+SELECT supplier.name, location.city, location.country
+FROM location
+LEFT OUTER JOIN supplier ON location.id = supplier.location_id"""
+    cursor.execute(sql1)
+    all_records01 = cursor.fetchall()
+    cursor.execute(sql2)
+    all_records02 = cursor.fetchall()
+    db.close()
+    #print(all_records)
+    print(f"The following products are missing suppliers:")
+    for record in all_records01:
+        if record[1] == None:
+            print(f"{record[0]}")
+    print(f"The following suppliers are missing products:")
+    for record in all_records01:
+        if record[0] == None:
+            print(f"{record[1]}")
+
+    print(f"The following locations are not associated with a supplier:")
+    for record in all_records02:
+        if record[0] == None:
+            print(f"{record[1]}", end=', ')
+            print(f"{record[2]}")
--- persistence/joins/main.py
+++ persistence/joins/main.py
@@ -6,6 +6,9 @@ def menu():
     print("[1] Display stock levels")
     print("[2] Display suppliers")
     print("[3] Display supplier locations")
+    print("[4] Display missing suppliers")
+    print("[5] Display missing products")
+    print("[6] Display missing data")
     selection = int(input("Your selection: "))
     return selection
 
@@ -18,6 +21,14 @@ def run():
         database.display_product_supplier()
     elif option == 3:
         database.display_product_supplier_locations()
+    elif option == 4:
+        database.display_products_missing_suppliers()
+    elif option == 5:
+        database.display_suppliers_missing_products()
+    elif option == 6:
+        database.display_missing_data()
+    else:
+        print("Invalid selection")
 
 
 if __name__ == '__main__':
